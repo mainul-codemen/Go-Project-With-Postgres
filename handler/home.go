dc129ac19ce5ce7308a3c2c90dd48a8c5c0ad7ee
maainul <mainul080@yahoo.com>
2021-06-17 13:20:55 +0600

    Create Struct and Show data to the html file

--- handler/home.go
+++ handler/home.go
@@ -3,16 +3,34 @@ package handler
 import (
 	"log"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
+type (
+	templateData struct {
+		Name string
+		Age  int
+	}
+)
+
 func (s *Server) getHome(w http.ResponseWriter, r *http.Request) {
-	tmp, _ := template.New("home.html").ParseFiles("./assets/templates/home.html")
+	funcMap := template.FuncMap{
+		"title": func(name string) string {
+			return strings.Title(name)
+		},
+	}
 
-	err := tmp.Execute(w, nil)
+	tmp, _ := template.New("home.html").Funcs(funcMap).ParseFiles("./assets/templates/home.html")
 
+	tmpData := templateData{
+		Name: "rahim karim",
+		Age:  23,
+	}
+
+	err := tmp.Execute(w, tmpData)
 	if err != nil {
-		log.Println("Error Executing template : ", err)
+		log.Println("Error executing template :", err)
 		return
 	}
 }
